49baa79d8d353cf9b7cd6316e14f71e463a78b8e
Swan AUVERGNE <swan.auvergne@gmail.com>
2023-12-11 01:30:17 +0100

    Comment décoration

--- décoration.js
+++ décoration.js
@@ -1,17 +1,12 @@
 const sapin = document.getElementById("sapin")
 
 function move_décorations() {
-    console.log("Flag")
-
     const left = sapin.offsetLeft
     const top = sapin.offsetTop
     const width = sapin.clientWidth
     const height = sapin.clientHeight
 
-    console.log(left, top, width, height)
-
     for (const décoration of document.querySelectorAll("img.décoration")) {
-        console.log(décoration)
         décoration.style.left = `${left + décoration.dataset.width*width}px`
         décoration.style.top = `${top + (1-décoration.dataset.height)*height}px`
         décoration.style.width = `${width * (décoration.dataset.scale_width || .07)}px`
